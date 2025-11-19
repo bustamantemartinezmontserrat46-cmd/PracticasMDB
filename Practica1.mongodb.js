345b6cebcdfaa0eb53c7f883e29f209d544be5b4
bustamantemartinezmontserrat46-cmd <bustamante.martinez.montserrat.46@cbtis118.edu.mx>
2025-11-19 18:26:05 +0000

    se hizo practica1

---
+++ Practica1.mongodb.js
@@ -0,0 +1,6 @@
+use ('Escuela');
+db.createCollection('profesores');
+db.createCollection('edificios');
+db.createCollection('salones');
+db.createCollection('materias');
+db.createCollection('alumnos');
